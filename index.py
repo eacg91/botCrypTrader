6cc6a80b52cebd2e2a157d10546b26fa28749ece
Edwin Alberto Castañeda García <eacg91@gmail.com>
2022-08-18 20:43:23 -0500

    Monitoreo de criptodivisas

--- index.py
+++ index.py
@@ -1,27 +1,73 @@
 import requests
+import time, os
 
-def convert(aValue,aCoinName,bCoinName):
-    aCoinName = aCoinName.upper()
-    bCoinName = bCoinName.upper()
+os.environ['TZ'] = 'UTC+6'
+#time.tzset()  #Linea bloqueada para funcionar en Termux (Android)
+
+class crypto():
+    #def __init__(self):
+    allBooks = {}
+
+def get_allBooks():
+    response = requests.get('https://api.bitso.com/v3/ticker/')
+    if response.json()['success'] :
+        #crypto.allBooks = response.json()['payload']
+        for x in range( len(response.json()['payload']) ):
+            i = response.json()['payload'][x]['book']
+            crypto.allBooks[i] = response.json()['payload'][x]
+        #print(crypto.allBooks[i])
+    return response.json()['success']
+
+def search_betweenTwoBooks( a,b,index ) :
+    aBooks = []
+    bBooks = []
+    c = ""
+    for x in index :
+        if a in x :
+            aBooks.append( x )
+        elif b in x:
+            bBooks.append( x )
+    if len(aBooks) > 0 and len(bBooks) > 0:
+        for x in aBooks :
+            xs0 = x.split('_')[0]
+            xs1 = x.split('_')[1]
+            for y in bBooks :
+                ys0 = y.split('_')[0]
+                ys1 = y.split('_')[1]
+                if xs0 == ys0 or xs0 == ys1 :
+                    c = xs0
+                elif xs1 == ys0 or xs1 == ys1 :
+                    c = xs1
+                if not c == "" :
+                    return [x,y,c]
+    x = y = "NO Encontrado"
+    return [x,y,c]
+
+def convert2(aValue,aCoinName,bCoinName):
+    aCoinName = aCoinName.lower()
+    bCoinName = bCoinName.lower()
     if not aCoinName == bCoinName :
-        book = aCoinName.lower()+'_'+bCoinName.lower()
-        response = requests.get('https://api.bitso.com/v3/ticker/?book={}'.format(book))
-        if response.json()['success'] :
-            json_response = response.json()
-            bValue = aValue * float(json_response['payload']['last'])
-        else :
-            book = bCoinName.lower()+'_'+aCoinName.lower()
-            response2 = requests.get('https://api.bitso.com/v3/ticker/?book={}'.format(book))
-            if response2.json()['success'] :
-                json_response = response2.json()
-                bValue = aValue / float(json_response['payload']['last'])
-            else :
-                print( response.json()['error'] )
-                print( response2.json()['error'] )
-                return False
+        book = aCoinName+'_'+bCoinName
+        try:
+            bValue = aValue * float(crypto.allBooks[book]['last'])
+        except:
+            book = bCoinName+'_'+aCoinName
+            try:
+                bValue = aValue / float(crypto.allBooks[book]['last'])
+            except:
+                cCoinName = search_betweenTwoBooks( aCoinName,bCoinName,crypto.allBooks )[2]
+                if not cCoinName == "" :
+                    print("*****")
+                    cValue = convert2(aValue,aCoinName,cCoinName)
+                    bValue = convert2(cValue,cCoinName,bCoinName)
+                    print("*****")
+                else : 
+                    print( "No se puede hacer trading de {} a {} usando Bitso !".format(aCoinName.upper(),bCoinName.upper()) )
+                    return 0
     else :
         bValue = aValue
-    print("{} {} = {} {}".format(aValue,aCoinName,bValue,bCoinName))
+    print(f"{aValue} {aCoinName.upper()} = {bValue} {bCoinName.upper()} ")
+    return bValue
 
 def get_address_balance(network,address) :
     response = requests.get('https://chain.so/api/v2/get_address_balance/{}/{}'.format(network,address))
@@ -36,14 +82,20 @@ def get_address_balance(network,address) :
     else:
         return response.json()['status']
 
-confirmed_balance = get_address_balance('BTC','14HprKtenqJWyMqT236J7i7DN5TArAiQLD')['confirmed_balance']
-convert(float(confirmed_balance),'BTC','MXN')
-convert(1,'APE','mxn')
-response = requests.get('https://api.bitso.com/v3/ticker/')
-allBooks = response.json()['payload']
-dateInfo = allBooks[0]['created_at']
-print(allBooks[0])
-print(dateInfo)
-for x in range( len(allBooks) ):
-    bookX = allBooks[x]['book'].upper()
-    print( "{}) {} = {} {} ".format(x+1,bookX,allBooks[x]['last'],bookX.split('_')[1]) )
+def test():
+	print( "Inicia programa {}".format(time.strftime("%c")) )
+	confirmed_balance = get_address_balance('BTC','14HprKtenqJWyMqT236J7i7DN5TArAiQLD')['confirmed_balance']
+	while get_allBooks() :
+		dateInfo = crypto.allBooks['btc_mxn']['created_at']
+		dateInfo = dateInfo.split('T')[0] +" "+ dateInfo.split('T')[1].split('+')[0]
+		print(f"\n*******\n Hora de Bitso {dateInfo}")
+		#print(crypto.allBooks['btc_mxn']) #imprime el JSON obtenido
+		convert2(1,'BTC','USD')
+		convert2(1,'ETH','USD')
+		convert2(1,'APE','USD')
+		convert2(1,'AAVE','USD')
+		convert2(1,'USD','MXN')
+		convert2(float(confirmed_balance),'BTC','mxn')
+		#for x in crypto.allBooks :
+		 #   print( "{}) 1 {} = {} {} ".format(x,x.split('_')[0].upper(),crypto.allBooks[x]['last'],x.split('_')[1].upper()) )
+		time.sleep(1)
