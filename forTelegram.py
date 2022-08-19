9122ac8f23de349b1d9b1383b14b1789296a8510
Edwin Alberto Castañeda García <eacg91@gmail.com>
2022-08-18 20:50:38 -0500

    testing telegramBot e importando monitor

---
+++ forTelegram.py
@@ -0,0 +1,32 @@
+from config import * #Importamos el token de config.py en el mismo directorio
+from index import *
+import telebot
+import threading
+
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
+@bot.message_handler(commands=["convert","convertir"])
+def cmd_start(message):
+	txt = " {} USD".format( convert2(1,"BTC",'USD') )
+	bot.reply_to(message,message.text.split(commands)[1])
+	
+@bot.message_handler(commands=["start","inicia"])
+def cmd_start(message):
+	bot.reply_to(message, "hola! ")
+
+@bot.message_handler(content_types=["text"])
+def bot_mensajes_texto(message):
+	if message.text.startswith("/"):
+		comand = message.text.split('/')[1]
+		#txt = "Dona Bitcoin si te gusta el programa"
+		txt = " {} USD".format( convert2(1,comand,'USD') )
+		#if command.startswith("convert") == ""
+		bot.send_message(message.chat.id,txt)
+
+crypto = crypto()
+hilo = threading.Thread(target=test)
+if __name__ == "__main__":
+	print("Iniciando Bot")
+	hilo.start()
+	bot.infinity_polling()
+	print("Fin")
