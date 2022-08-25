ffed5b2413cf9c804d84a4af0eca6145b48fe7c0
Edwin Alberto Castañeda García <eacg91@gmail.com>
2022-08-24 23:31:46 -0500

    config.py

---
+++ example_config.py
@@ -0,0 +1,3 @@
+#Debes renombrar este archivo como "config.py" / You need to rename this file like "config.py"
+#config.py está en la lista .gitignore para ser ignorado en la sincronización con git / config.py is in .gitignore list for be ignored when sincronized with git
+TELEGRAM_TOKEN = "TelegramToken"
